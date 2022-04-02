e8bcc7938372193a328264a736a44df7a00fe1a9
Nikhil Nishad <nikhilnishad279@gmail.com>
2022-04-02 20:44:10 +0530

    discovery service added

---
+++ PhotoAppDiscoveryService/src/test/java/com/nikhilnishad/discoveryService/PhotoAppdiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhilnishad.discoveryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppdiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
