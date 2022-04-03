c6e14e99ce03defb75b92d1503f3940539e230fa
Nikhil Kr. Nishad <nikhilnishad279@gmail.com>
2022-04-03 16:58:33 +0530

    remaining files from previous commit

---
+++ PhotoAppAccountManagementService/src/test/java/com/nikhilnishad/photoApp/accountManagementService/PhotoAppAccountManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhilnishad.photoApp.accountManagementService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppAccountManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
