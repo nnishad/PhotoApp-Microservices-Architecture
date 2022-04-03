9c7bf8e8bd555cf2baf2319bc595190151f24ea2
Nikhil Kr. Nishad <nikhilnishad279@gmail.com>
2022-04-03 17:53:50 +0530

    Autowired Environment to check running port

--- PhotoAppAccountManagementService/src/main/java/com/nikhilnishad/photoApp/accountManagementService/ui/controllers/AccountController.java
+++ PhotoAppAccountManagementService/src/main/java/com/nikhilnishad/photoApp/accountManagementService/ui/controllers/AccountController.java
@@ -1,5 +1,7 @@
 package com.nikhilnishad.photoApp.accountManagementService.ui.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/account")
 public class AccountController {
 
+	@Autowired
+	private Environment env;
+	
 	@GetMapping("/status")
 	public String status() {
-		return "working";
+		return "running on port "+env.getProperty("local.server.port");
 	}
 }
--- PhotoAppUserService/src/main/java/com/nikhilnishad/photoApp/userService/ui/controllers/UsersController.java
+++ PhotoAppUserService/src/main/java/com/nikhilnishad/photoApp/userService/ui/controllers/UsersController.java
@@ -1,16 +1,23 @@
 package com.nikhilnishad.photoApp.userService.ui.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.discovery.converters.Auto;
 
 @RestController
 @RequestMapping("/users")
 public class UsersController {
 	
+	@Autowired
+	private Environment env;
+	
 	@GetMapping("/status")
 	public String status() {
-		return "working";
+		return "running on port "+env.getProperty("local.server.port");
 	}
 
 }
