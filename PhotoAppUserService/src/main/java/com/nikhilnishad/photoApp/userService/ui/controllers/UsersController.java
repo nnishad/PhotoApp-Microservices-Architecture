27045a90fb270ac92f300d5748ad1e1359c470c6
Nikhil Kr. Nishad <nikhilnishad279@gmail.com>
2022-04-03 16:05:17 +0530

    Users microservice added with eureka client dependency and dynamic port
    Signed-off-by: Nikhil Kr. Nishad <nikhilnishad279@gmail.com>

---
+++ PhotoAppUserService/src/main/java/com/nikhilnishad/photoApp/userService/ui/controllers/UsersController.java
@@ -0,0 +1,16 @@
+package com.nikhilnishad.photoApp.userService.ui.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+	
+	@GetMapping("/status")
+	public String status() {
+		return "working";
+	}
+
+}
---
+++ PhotoAppUserService/src/test/java/com/nikhilnishad/photoApp/userService/PhotoAppUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhilnishad.photoApp.userService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
