dcf76511a0a1d4ce69100c6d4884460d2ed21288
Nikhil Kr. Nishad <nikhilnishad279@gmail.com>
2022-04-03 17:37:59 +0530

    package name update

--- PhotoAppDiscoveryService/src/main/java/com/nikhilnishad/photoApp/discoveryService/PhotoAppdiscoveryServiceApplication.java
+++ PhotoAppDiscoveryService/src/main/java/com/nikhilnishad/photoApp/discoveryService/PhotoAppdiscoveryServiceApplication.java
@@ -1,4 +1,4 @@
-package com.nikhilnishad.discoveryService;
+package com.nikhilnishad.photoApp.discoveryService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
