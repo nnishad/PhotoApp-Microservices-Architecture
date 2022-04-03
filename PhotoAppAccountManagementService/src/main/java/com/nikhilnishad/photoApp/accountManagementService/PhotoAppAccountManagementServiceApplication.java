eb2d788c3d10e0a3eb99c53039c1729d2abf4c0a
Nikhil Kr. Nishad <nikhilnishad279@gmail.com>
2022-04-03 16:57:45 +0530

    Account management service with eureka client and dynamic port
    Signed-off-by: Nikhil Kr. Nishad <nikhilnishad279@gmail.com>

---
+++ PhotoAppAccountManagementService/src/main/java/com/nikhilnishad/photoApp/accountManagementService/PhotoAppAccountManagementServiceApplication.java
@@ -0,0 +1,15 @@
+package com.nikhilnishad.photoApp.accountManagementService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PhotoAppAccountManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppAccountManagementServiceApplication.class, args);
+	}
+
+}
