7fdef1aaa962ffdbbf24b6578c4e8117dd1ab588
Nikhil Kr. Nishad <nikhilnishad279@gmail.com>
2022-04-03 17:37:22 +0530

    API gateway added
    Application.properties have auto-registering with eureka server and lowercase -serive id enabled

---
+++ PhotoAppAPIGateway/src/test/java/com/nikhilnishad/photoApp/gateway/PhotoAppApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikhilnishad.photoApp.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
