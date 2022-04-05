6dc320887db6d5a3fc798e40fdd7cbaef7d40150
Nikhil Kr. Nishad <nikhilnishad279@gmail.com>
2022-04-05 14:56:37 +0530

    consume and produce added to post endpoint

--- PhotoAppUserService/src/main/java/com/nikhilnishad/photoApp/userService/ui/controllers/UsersController.java
+++ PhotoAppUserService/src/main/java/com/nikhilnishad/photoApp/userService/ui/controllers/UsersController.java
@@ -5,6 +5,7 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,7 +34,7 @@ public class UsersController {
 	}
 	
 	
-	@PostMapping
+	@PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE},produces= {MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE})
 	public ResponseEntity<CreateUserResponseModel> createUser(@RequestBody CreateUserRequestModel userDetails) {
 		
 		ModelMapper modelMapper=new ModelMapper();
