23ade025be1ede9bae9bf91118e34036c1baf9c3
Nikhil Kr. Nishad <nikhilnishad279@gmail.com>
2022-04-05 14:49:15 +0530

    Added spring security
    implemented for password encryption
    to allow requests only from gateway ip.

--- PhotoAppUserService/src/main/java/com/nikhilnishad/photoApp/userService/PhotoAppUserServiceApplication.java
+++ PhotoAppUserService/src/main/java/com/nikhilnishad/photoApp/userService/PhotoAppUserServiceApplication.java
@@ -3,6 +3,8 @@ package com.nikhilnishad.photoApp.userService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +12,11 @@ public class PhotoAppUserServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PhotoAppUserServiceApplication.class, args);
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
--- PhotoAppUserService/src/main/java/com/nikhilnishad/photoApp/userService/service/Impl/UsersServiceImpl.java
+++ PhotoAppUserService/src/main/java/com/nikhilnishad/photoApp/userService/service/Impl/UsersServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.nikhilnishad.photoApp.userService.data.UserEntity;
@@ -18,14 +19,17 @@ public class UsersServiceImpl implements UsersService {
 	@Autowired
 	UsersRepository userRepository;
 	
+	@Autowired
+	BCryptPasswordEncoder bCryptPasswordEncoder; 
+	
 	@Override
 	public UserDto createUser(UserDto userDetails) {
 		// TODO Auto-generated method stub
 		userDetails.setUserId(UUID.randomUUID().toString());
+		userDetails.setEncryptedPassword(bCryptPasswordEncoder.encode(userDetails.getPassword()));
 		ModelMapper modelMapper=new ModelMapper();
 		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 		UserEntity userEntity=modelMapper.map(userDetails, UserEntity.class);
-		userEntity.setEncryptedPassword("test");
 		UserDto createdUser=modelMapper.map(userRepository.save(userEntity),UserDto.class);
 		return createdUser;
 	}
