2a365e8e24bd6a2e79f7039c22ba1d3fa2f027e5
Nikhil Kr. Nishad <nikhilnishad279@gmail.com>
2022-04-06 14:07:32 +0530

    order added to filters

--- PhotoAppAPIGateway/src/main/java/com/nikhilnishad/photoApp/gateway/GlobalFiltersConfiguration.java
+++ PhotoAppAPIGateway/src/main/java/com/nikhilnishad/photoApp/gateway/GlobalFiltersConfiguration.java
@@ -5,12 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 
 import reactor.core.publisher.Mono;
 
 @Configuration
 public class GlobalFiltersConfiguration {
 	final Logger logger= LoggerFactory.getLogger(GlobalFiltersConfiguration.class);
+	@Order(1)
 	@Bean
 	public GlobalFilter secondPreFilter() {
 		return (exchnage,chain)->{
@@ -22,6 +24,7 @@ public class GlobalFiltersConfiguration {
 		};
 	}
 	
+	@Order(2)
 	@Bean
 	public GlobalFilter thirdPreFilter() {
 		return (exchnage,chain)->{
